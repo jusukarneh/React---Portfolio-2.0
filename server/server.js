35b319041dcfeb7688530f42fb566048c7a10a8c
jusukarneh <jusudaddi@yahoo.com>
2024-03-24 22:15:55 -0400

    path

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const nodemailer = require('nodemailer')
- 
+ const path = require('path')
 require("dotenv").config() 
 app.use(express.urlencoded({extended:true})) 
 app.use(express.json())
